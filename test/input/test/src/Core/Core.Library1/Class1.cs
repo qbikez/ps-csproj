b7d05eda3c58ef1867758e623fcd52fa066312a1
jakub.pawlowski <jakub.pawlowski@legimi.com>
2016-02-13 23:16:28 +0100

    add a test solution

---
+++ test/input/test/src/Core/Core.Library1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Library1
+{
+    public class Class1
+    {
+        public string Foo(string msg)
+        {
+            return $"bar {msg}";
+        }
+    }
+}
