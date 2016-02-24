058f413c26bef8542273d6e41c99edd558b23a4f
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-02-24 10:50:03 +0100

    add a test library2 with nuspec and nupkg

---
+++ test/input/test/src/Core/Core.Library2/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Library2
+{
+    public class Class2
+    {
+        public string Bar(string msg)
+        {
+            return $"foobar {msg}";
+        }
+    }
+}
