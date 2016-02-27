32397e5d53fa9c7d40327c7ebd1bba7bea7f24d5
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-02-27 19:49:10 +0100

    update test project - one project reference and one nuget

--- test/input/test/src/Core/Core.Library1/Properties/AssemblyInfo.cs
+++ test/input/test/src/Core/Core.Library1/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Core.Library1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("library1")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("testcompany")]
 [assembly: AssemblyProduct("Core.Library1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
