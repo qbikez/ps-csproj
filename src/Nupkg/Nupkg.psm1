039299cb5270a538eef6be3e10d0076899f0ca31
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-03-15 09:34:26 +0100

    update nupkg module

--- src/Nupkg/Nupkg.psm1
+++ src/Nupkg/Nupkg.psm1
@@ -1,5 +1,20 @@
 ipmo process
 
+$root = "."
+if (![string]::IsNullOrEmpty($PSScriptRoot)) {
+    $root = $PSScriptRoot
+}
+#if ($MyInvocation.MyCommand.Definition -ne $null) {
+#    $root = $MyInvocation.MyCommand.Definition
+#}
+$helpersPath = $root
+@("choco-utils.ps1", "internal.ps1") |
+    % { 
+        . "$root/functions/$_" 
+    }
+
+
+
 function find-nugetdll {
     param (
         [Parameter(Mandatory=$true)] $name, 
@@ -23,27 +38,6 @@ function find-nugetdll {
     return $null
 }
 
-function sort-frameworks([Parameter(ValueFromPipeline=$true)] $frameworks, $frameworkhint) {
-begin {
-    $ordered = @()
-}
-process {
-    $order = switch($_.name) {
-        $frameworkhint { 0; break }
-        {$_ -match "^net" } { 10; break }
-        {$_ -match "^dnx" }  { 20; break }
-        default { 100; break }
-    }
-    $ordered += @( New-Object -type pscustomobject -Property @{
-            dir = $_
-            order = $order
-        })
-}
-end {
-    $ordered = $ordered | sort dir -Descending | sort order 
-    return $ordered | select -ExpandProperty dir
-    }
-}
 
 function get-nugetname {
     param($name)
---
+++ src/Nupkg/functions/internal.ps1
@@ -0,0 +1,21 @@
+function sort-frameworks([Parameter(ValueFromPipeline=$true)] $frameworks, $frameworkhint) {
+begin {
+    $ordered = @()
+}
+process {
+    $order = switch($_.name) {
+        $frameworkhint { 0; break }
+        {$_ -match "^net" } { 10; break }
+        {$_ -match "^dnx" }  { 20; break }
+        default { 100; break }
+    }
+    $ordered += @( New-Object -type pscustomobject -Property @{
+            dir = $_
+            order = $order
+        })
+}
+end {
+    $ordered = $ordered | sort dir -Descending | sort order 
+    return $ordered | select -ExpandProperty dir
+    }
+}
