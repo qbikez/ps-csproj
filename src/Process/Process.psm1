8d4add1429f77c6c28ca1bcb199843c0f8a7b04f
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-04-29 08:29:17 +0200

    invoke: add $in parameter for passing input

--- src/Process/Process.psm1
+++ src/Process/Process.psm1
@@ -16,27 +16,37 @@ function write-indented ([Parameter(ValueFromPipeline=$true, Position=1)]$msg, [
         @($_.ToString().split("`n")) | % {
             $msg = $_
             $idx = 0    
-    while($idx -lt $msg.length) {
-        $chunk = [System.Math]::Min($msg.length - $idx, $maxlen)
-        $chunk = [System.Math]::Max($chunk, 0)
-        write-host "$pad$($msg.substring($idx,$chunk))"
-        $idx += $chunk
-    }
-}
+            while($idx -lt $msg.length) {
+                $chunk = [System.Math]::Min($msg.length - $idx, $maxlen)
+                $chunk = [System.Math]::Max($chunk, 0)
+                write-host "$pad$($msg.substring($idx,$chunk))"
+                $idx += $chunk
+            }
+        }
     }
 }
 
 
-function invoke($command, [string[]]$arguments, [switch][bool]$nothrow, [switch][bool]$showoutput) {
+function invoke(
+    [parameter(Mandatory=$true)]$command,  
+    [parameter(ValueFromRemainingArguments=$true)][string[]]$arguments, 
+    [switch][bool]$nothrow, 
+    [switch][bool]$showoutput,
+    $in) {
     if ($showoutput) {
-        $o = & $command $arguments 2>&1 | write-indented 2
+        $o = $in | & $command $arguments 2>&1| write-indented -level 2
     } else {
-        $o = & $command $arguments 2>&1
+        $o = $in | & $command $arguments 2>&1
     }
     if ($lastexitcode -ne 0) {
-        $o | out-string | write-host
+        if (!$nothrow) {
+            $o | out-string | write-error 
+        } else {
+           $o | out-string | write-host 
+        }
     }
     if (!$nothrow -and $lastexitcode -ne 0) {
         throw "Command returned $lastexitcode"
     }
+    return $o
 }
