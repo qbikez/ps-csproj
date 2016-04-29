ff6d40cb1c90b41c300f8e38a83860da722d2d01
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-04-29 08:28:51 +0200

    write-indented: handle pipeline input, handle multiline strings

--- src/Process/Process.psm1
+++ src/Process/Process.psm1
@@ -1,6 +1,6 @@
 
 #todo: move this to logging module
-function write-indented ($level, $msg, $mark = "> ", $maxlen) {
+function write-indented ([Parameter(ValueFromPipeline=$true, Position=1)]$msg, [Parameter(Position=2)]$level, [Parameter(Position=3)]$mark = "> ", [Parameter(Position=4)]$maxlen) {
     $pad = $mark.PadLeft($level)
     if ($maxlen -eq $null) {
         if ($host.UI.RawUI.WindowSize.Width -gt 0) {
@@ -10,13 +10,19 @@ function write-indented ($level, $msg, $mark = "> ", $maxlen) {
             $maxlen = 512
         }
     }
-    $idx = 0
     
+    $msgs = @($msg)
+    $msgs | % {        
+        @($_.ToString().split("`n")) | % {
+            $msg = $_
+            $idx = 0    
     while($idx -lt $msg.length) {
         $chunk = [System.Math]::Min($msg.length - $idx, $maxlen)
         $chunk = [System.Math]::Max($chunk, 0)
         write-host "$pad$($msg.substring($idx,$chunk))"
         $idx += $chunk
+    }
+}
     }
 }
 
