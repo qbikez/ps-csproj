ef34758324c9d353cd8488f5c568bbc69f8ee110
jakub.pawlowski <jakub.pawlowski@legimi.com>
2016-09-04 18:16:01 +0200

    process: unify line endings

--- test/process.tests.ps1
+++ test/process.tests.ps1
@@ -5,9 +5,9 @@ Describe "Process module tests" {
     # init - download echoargs
     if ((get-command echoargs -erroraction "Ignore") -eq $null) {
         nuget install echoargs -version 3.2.0 -source https://chocolatey.org/api/v2/ -outputdirectory .tools
-        $env:Path = $env:Path + ";$((get-item '.tools/echoargs.3.2.0/tools').FullName)"
+        $env:Path = "$((get-item '.tools/echoargs.3.2.0/tools').FullName);" + $env:Path 
     }
-
+    $echoargs = (get-command echoargs).Source -replace "chocolatey\\bin\\EchoArgs.exe","chocolatey\lib\echoargs\tools\EchoArgs.exe"
     It "Should invoke process" {
         invoke echoargs
     }
@@ -15,16 +15,16 @@ Describe "Process module tests" {
         { invoke "not-found.exe" } | should throw
     }#>
     It "Should pass all direct arguments" {
-        $r = invoke echoargs -a=1 -b 2 -e="1 2" -passthru -verbose | out-string
+        $r = invoke echoargs -a=1 -b 2 -e="1 2" -passthru -verbose | out-string | % { $_ -replace "`r`n","`n" }
         $expected = @"
 Arg 0 is <-a=1>
 Arg 1 is <-b>
 Arg 2 is <2>
 Arg 3 is <-e=1 2>
 Command line:
-"c:\tools\chocolatey\lib\echoargs\tools\EchoArgs.exe" -a=1 -b 2 "-e=1 2"
+"$echoargs" -a=1 -b 2 "-e=1 2"
 
-"@
+"@ | % { $_ -replace "`r`n","`n" }
         $r | Should Be $expected
 
     }
@@ -36,16 +36,16 @@ Command line:
             "2"
             "-e=1 2"
         )
-        $r = invoke echoargs -arguments $a -passthru | out-string
+        $r = invoke echoargs -arguments $a -passthru | out-string  | % { $_ -replace "`r`n","`n" }
         $expected = @"
 Arg 0 is <-a=1>
 Arg 1 is <-b>
 Arg 2 is <2>
 Arg 3 is <-e=1 2>
 Command line:
-"c:\tools\chocolatey\lib\echoargs\tools\EchoArgs.exe" -a=1 -b 2 "-e=1 2"
+"$echoargs" -a=1 -b 2 "-e=1 2" 
 
-"@
+"@ | % { $_ -replace "`r`n","`n" }
         $r | Should Be $expected
 
     }
@@ -56,16 +56,16 @@ Command line:
             "2"
             "-e=`"1 2`""
         )
-        $r = invoke echoargs -arguments $a -passthru | out-string
+        $r = invoke echoargs -arguments $a -passthru | out-string  | % { $_ -replace "`r`n","`n" }
         $expected = @"
 Arg 0 is <-a=1>
 Arg 1 is <-b>
 Arg 2 is <2>
 Arg 3 is <-e=1 2>
 Command line:
-"c:\tools\chocolatey\lib\echoargs\tools\EchoArgs.exe" -a=1 -b 2 -e="1 2"
+"$echoargs" -a=1 -b 2 -e="1 2"
 
-"@
+"@  | % { $_ -replace "`r`n","`n" }
         $r | Should Be $expected
 
     }
@@ -75,15 +75,15 @@ Command line:
             "-b"
             "2"
         )
-        $r = invoke echoargs $a -passthru | out-string
+        $r = invoke echoargs $a -passthru | out-string  | % { $_ -replace "`r`n","`n" }
         $expected = @"
 Arg 0 is <-a=1>
 Arg 1 is <-b>
 Arg 2 is <2>
 Command line:
-"c:\tools\chocolatey\lib\echoargs\tools\EchoArgs.exe" -a=1 -b 2
+"$echoargs" -a=1 -b 2
 
-"@
+"@  | % { $_ -replace "`r`n","`n" }
         $r | Should Be $expected
 
     }
