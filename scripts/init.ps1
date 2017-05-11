d433428184306d0b3661998b594ae4451b10f532
jakub.pawlowski <pawlowskijakub@gmail.com>
2017-05-11 11:22:38 +0200

    init: try to install dotnetcore versions side by side

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -1,3 +1,6 @@
 & "$PSScriptRoot\lib\init.ps1"
 
-choco install dotnetcore-sdk -y --version 1.1.1
+
+# sdk 1.1.1 is required for powerecho compilation
+choco install dotnetcore-sdk -y --sxs --version 1.0.0-RC2
+choco install dotnetcore-sdk -y --sxs --version 1.1.1
