852d907f8aafb82e675392552907a17678edda8f
jakub.pawlowski <pawlowskijakub@gmail.com>
2017-05-11 12:14:39 +0200

    match dotnet sdk versions in global.json with those installed by choco

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -2,5 +2,5 @@
 
 
 # sdk 1.1.1 is required for powerecho compilation
-choco install dotnetcore-sdk -y --sxs --version 1.0.0-RC2
+choco install dotnetcore-sdk -y --sxs --version 1.0.0-RC2 # this corresponds to sdk version 1.0.0-preview1-002702
 choco install dotnetcore-sdk -y --sxs --version 1.1.1
