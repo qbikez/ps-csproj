9da2e22445ea468b545ec4c837db03a7aaf338c1
jakub.pawlowski <pawlowskijakub@gmail.com>
2020-07-19 07:50:36 +0200

    check for dotnet sdk version before installing it

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -1,9 +1,20 @@
 & "$PSScriptRoot\lib\init.ps1"
 
-if (!(get-command nuget)) {
+if (!(get-command nuget -ErrorAction ignore)) {
     choco install -y nuget.commandline
 }
 
-choco install dotnetcore-sdk -y --sxs --version 3.1.300
+$dotnetsdk = $null
+if ((get-command dotnet -ErrorAction ignore)) {
+    $sdks = dotnet --list-sdks
+    $matching = $sdks | ? { $_.Trim().StartsWith("3.1.") }
+    if ($matching) {
+        $dotnetsdk = @($matching) | select -first 1
+    }
+}
+if (!$dotnetsdk) {
+    write-host "dotnet sdk version 3.1.* not found. Installing..."
+    choco install dotnetcore-sdk -y --sxs --version 3.1.300
+}
 
 & "$PSScriptRoot\build"
