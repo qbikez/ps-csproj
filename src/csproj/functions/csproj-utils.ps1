954a2be8914c9ec9707634970a49f684812d079b
jakub.pawlowski <pawlowskijakub@gmail.com>
2020-07-19 08:51:42 +0200

    lib

--- src/csproj/functions/csproj-utils.ps1
+++ src/csproj/functions/csproj-utils.ps1
@@ -39,6 +39,7 @@ public class ReferenceMeta {
 }
 "@
 
+
 if (-not ([System.Management.Automation.PSTypeName]'Csproj').Type) {
     add-type -TypeDefinition $types -ReferencedAssemblies "System.Xml","System.Xml.ReaderWriter","System.IO","System.Runtime.Extensions"
 }
